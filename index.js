2d37fd1930a5ea5d88036cb49a360d210d229730
fortune710 <fortune0208@yahoo.com>
2022-07-20 14:52:56 +0100

    added routes for server

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const bodyparser = require('body-parser').json()
+
+
+const { AuthRouter } = require('./api/v1/routes/Auth')
+const { FollowRouter } = require('./api/v1/routes/FollowMosque')
+const { MosqueRouter } = require('v1/routes/Mosque');
+const { AdminRouter } = require('v1/routes/MosqueAdmin');
+const { PostRouter } = require('v1/routes/Post');
+const { PrayerTimeRouter } = require('v1/routes/PrayerTime');
+
 const app = express()
+
 
 app.use(bodyparser)
 app.get('/', (req, res) => {
     res.send("Working")
 })
-const PORT = 3000;
+
+app.use('/auth', AuthRouter)
+app.use('/following', FollowRouter)
+app.use('/mosque', MosqueRouter)
+app.use('/admin', AdminRouter)
+app.use('/post', PostRouter)
+app.use('/prayer-time', PrayerTimeRouter)
+
+const PORT = 4000;
 
 app.listen(process.env.PORT || PORT)
