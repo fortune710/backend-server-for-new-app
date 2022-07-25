b628332dda995acd0f63bf36bd667685c0141b03
fortune710 <fortune0208@yahoo.com>
2022-07-25 11:15:49 +0100

    changes

--- api/config/variables.js
+++ api/config/variables.js
@@ -1,7 +1,7 @@
 const DB_CONFIG = {
     DB_HOST: 'ec2-54-225-234-165.compute-1.amazonaws.com',
     DB_USERNAME: 'bcnhffkzijjkcp',
-    DB_NAME: 'bcnhffkzijjkcp',
+    DB_NAME: 'dc7fbq8m1oosq1',
     DB_PASSWORD: '7ee4e148e08e21c78004a2a246cce1cc3b78e40d0062733e910263c1bb45abef'
 }
 
