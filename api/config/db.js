bfc1bea4ca97cd20fd071e6e0c0b3c8746b3db28
fortune710 <fortune0208@yahoo.com>
2022-08-18 11:22:04 +0100

    changed db config for sql database

--- api/config/db.js
+++ api/config/db.js
@@ -2,6 +2,8 @@ const { Sequelize } = require('sequelize')
 const DB_CONFIG = require('./variables')
 require('dotenv').config()
 
+
+/*
 const { DB_HOST, DB_NAME, DB_PASSWORD, DB_USERNAME } = DB_CONFIG
 
 const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD , {
@@ -16,6 +18,21 @@ const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD , {
             }
         }
     })
+})
+*/
+
+const databaseConfig = {
+    databaseName: 'sql5513562',
+    username: 'sql5513562',
+    password: 'YiICPYrllR',
+    host: 'sql5.freesqldatabase.com',
+    port: '3306'
+}
+
+const sequelize = new Sequelize(databaseConfig.databaseName, databaseConfig.username, databaseConfig.password, {
+    host: databaseConfig.host,
+    dialect: 'mysql',
+    port: databaseConfig.port
 })
 
 sequelize.authenticate()
