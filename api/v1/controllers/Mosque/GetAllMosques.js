56ee42b1e657cc5a8c7d2d064a31a80199d505f9
fortune710 <fortune0208@yahoo.com>
2022-08-21 23:04:23 +0100

    added coordinates support

---
+++ api/v1/controllers/Mosque/GetAllMosques.js
@@ -0,0 +1,33 @@
+const { Mosque } = require('../../../models/Mosque');
+const { Op } = require('sequelize');
+
+
+const GetAllMosques = async(req, res) => {
+    const { longitude, latitude } = req.params;
+
+    if(longitude && latitude){
+        return
+    } else if((!longitude&&latitude)||(longitude&&!latitude)){
+        return res.json({ response: "One coordinate is missing" })
+    } else {
+        return await Mosque.findAll({
+            where: {
+                still_exists: true,
+                registered_by: {
+                    [Op.not]: null
+                } 
+            }
+        })
+        .then(data => {
+            res.json({ response: data })
+        })
+        .catch(err => {
+            res.json({ response: "Could not get all mosques!" })
+        })
+    }
+}
+
+
+module.exports = {
+    GetAllMosques: GetAllMosques
+}
