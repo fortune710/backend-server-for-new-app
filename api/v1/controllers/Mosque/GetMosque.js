38ea125d2d574758e5856513180349a231aa9730
fortune710 <fortune0208@yahoo.com>
2022-08-21 23:03:45 +0100

    gets more details from mosque

--- api/v1/controllers/Mosque/GetMosque.js
+++ api/v1/controllers/Mosque/GetMosque.js
@@ -1,14 +1,16 @@
 const { Mosque } = require('../../../models/Mosque');
 
 const GetMosque = async(req, res) => {
-    if(req instanceof Request){
-        const { id } = req.params;
-        if(!id){
-            res.json({ response:'Data missing!' })
-            return
-        } else {
-            await Mosque.findByPk(id).then(data => res.json({ response: data }))
-            .catch(() => res.json({ response:'Could not get mosque' }))
-        }
+    const { id } = req.params;
+    if(!id){
+        res.json({ response:'Data missing!' })
+        return
+    } else {
+        return await Mosque.findByPk(id).then(data => res.json({ response: data }))
+        .catch(() => res.json({ response:'Could not get mosque' }))
     }
+}
+
+module.exports = {
+    GetMosque: GetMosque
 }
