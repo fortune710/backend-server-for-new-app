7174ad11178d4ab6bc1a8d781152dc287585c6af
fortune710 <fortune0208@yahoo.com>
2022-08-21 23:06:04 +0100

    check if email exists

--- api/v1/controllers/Mosque/AddMosque.js
+++ api/v1/controllers/Mosque/AddMosque.js
@@ -1,17 +1,54 @@
 const { Mosque } = require('../../../models/Mosque');
 const { makeid } = require('../../../../helpers/randomid');
+const {MosqueAccount} = require('../../../models/MosqueAccount');
+
+async function checkIfEmailExists(email){
+    const exists = await MosqueAccount.count({
+        where: {
+            email: email
+        }
+    })
+    .then(res => {
+        if(res > 0)
+            return true
+        else
+            return false
+    })
+    return exists
+}
+
 
 const AddMosque = async(req, res) => {
 
+    const { email, password, name, longitude, latitude, registered_by } = req.body;
+    const { imam_name, address, denomination, phone_number } = req.body;
+    
+    const emailExists = await checkIfEmailExists(email)
     if(!req.body){
         res.json({ response:'Data missing!' })
         return
+    } else if(emailExists){
+        return res.json({ response: "Email already in use" })
     } else {
         await Mosque.create({
             id: makeid(20),
-            ...req.body
+            name: name,
+            address: address,
+            longitude: longitude,
+            latitude: latitude,
+            imam_name: imam_name,
+            denomination: denomination,
+            phone_number: phone_number,
+            registered_by: registered_by
         })
-        .then(data => res.json({ response:data }))
+        .then(async(data) => {
+            res.json({ response:data })
+            await MosqueAccount.create({
+                mosque_id: data.id,
+                email: email,
+                password: password
+            })
+        })
         .catch(() => res.json({ response:'Error while adding mosque!', body: req.body }))
     }
 
