40e488cde8ceebb74ba19eb8e21c3e0b34ce0cf9
fortune710 <fortune0208@yahoo.com>
2022-08-23 12:12:17 +0100

    changes'

--- api/v1/controllers/Mosque/GetAllMosques.js
+++ api/v1/controllers/Mosque/GetAllMosques.js
@@ -5,26 +5,29 @@ const { Op } = require('sequelize');
 const GetAllMosques = async(req, res) => {
     const { longitude, latitude } = req.params;
 
+    return await Mosque.findAll({
+        where: {
+            still_exists: true,
+            registered_by: {
+                [Op.not]: null
+            } 
+        }
+    })
+    .then(data => {
+        res.json({ response: data })
+    })
+    .catch(err => {
+        res.json({ response: "Could not get all mosques!" })
+    })
+
+    /*
     if(longitude && latitude){
         return
     } else if((!longitude&&latitude)||(longitude&&!latitude)){
         return res.json({ response: "One coordinate is missing" })
     } else {
-        return await Mosque.findAll({
-            where: {
-                still_exists: true,
-                registered_by: {
-                    [Op.not]: null
-                } 
-            }
-        })
-        .then(data => {
-            res.json({ response: data })
-        })
-        .catch(err => {
-            res.json({ response: "Could not get all mosques!" })
-        })
-    }
+        
+    }*/
 }
 
 
