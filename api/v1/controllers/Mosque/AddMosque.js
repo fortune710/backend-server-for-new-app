a3cb3aa98c46c7d7fef99773bea5fbc10d6b2120
fortune710 <fortune0208@yahoo.com>
2022-08-24 03:18:45 +0100

    add times directly from create mosque route

--- api/v1/controllers/Mosque/AddMosque.js
+++ api/v1/controllers/Mosque/AddMosque.js
@@ -1,6 +1,7 @@
 const { Mosque } = require('../../../models/Mosque');
 const { makeid } = require('../../../../helpers/randomid');
 const {MosqueAccount} = require('../../../models/MosqueAccount');
+const { PrayerTime } = require('../../../models/PrayerTime');
 
 async function checkIfEmailExists(email){
     const exists = await MosqueAccount.count({
@@ -17,11 +18,25 @@ async function checkIfEmailExists(email){
     return exists
 }
 
+async function setPrayerObject(prayer, mosque_id){
+    const { id, time } = prayer;
+    const object = {
+        prayer_id: id,
+        mosque_id: mosque_id,
+        start_time: time.iqama,
+        call_time: time.azan
+    }
+    return await PrayerTime.create(object)
+}
+
 
 const AddMosque = async(req, res) => {
 
     const { email, password, name, longitude, latitude, registered_by } = req.body;
-    const { imam_name, address, denomination, phone_number } = req.body;
+    const { imam_name, address, denomination, phone_number, prayers } = req.body;
+
+    const { fajr, maghrib, isha, asr, dhuhr, jumaat } = prayers
+
     
     const emailExists = await checkIfEmailExists(email)
     if(!req.body){
@@ -42,12 +57,23 @@ const AddMosque = async(req, res) => {
             registered_by: registered_by
         })
         .then(async(data) => {
-            res.json({ response:data })
+            const prayerTimes = await Promise.all([ setPrayerObject(fajr, mosque_id), setPrayerObject(maghrib, mosque_id), 
+                setPrayerObject(isha, mosque_id), setPrayerObject(asr, mosque_id),
+                setPrayerObject(dhuhr, mosque_id), setPrayerObject(jumaat, mosque_id) ])
+                .then(data => { return data })
+            
             await MosqueAccount.create({
                 mosque_id: data.id,
                 email: email,
                 password: password
             })
+
+            const mosqueData = {
+                ...data.dataValues,
+                prayers: prayerTimes
+            }
+
+            return res.json({ response: mosqueData })
         })
         .catch(() => res.json({ response:'Error while adding mosque!', body: req.body }))
     }
