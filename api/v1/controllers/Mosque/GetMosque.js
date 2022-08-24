54f314c0fea936078e8f0f140def231d06e8af06
fortune710 <fortune0208@yahoo.com>
2022-08-24 03:18:28 +0100

    can get prayer times from get mosque route

--- api/v1/controllers/Mosque/GetMosque.js
+++ api/v1/controllers/Mosque/GetMosque.js
@@ -1,12 +1,34 @@
 const { Mosque } = require('../../../models/Mosque');
+const { PrayerTime } = require('../../../models/PrayerTime');
+
+
+const getPrayerTimes = async (id) => {
+    try {
+        return await PrayerTime.findAll({
+            where: {
+                mosque_id: id
+            }
+        })
+    } catch (err) {
+        return []
+    }
+}
 
 const GetMosque = async(req, res) => {
     const { id } = req.params;
+    const prayers = await getPrayerTimes(id);
+
     if(!id){
         res.json({ response:'Data missing!' })
         return
     } else {
-        return await Mosque.findByPk(id).then(data => res.json({ response: data }))
+        return await Mosque.findByPk(id).then(data => {
+            const mosqueData = {
+                ...data.dataValues,
+                prayer_times: prayers
+            }
+            res.json({ response: mosqueData })
+        })
         .catch(() => res.json({ response:'Could not get mosque' }))
     }
 }
