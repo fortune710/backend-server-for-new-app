64e6a9adf3d1c372d34f161960b0434cf3d83537
fortune710 <fortune0208@yahoo.com>
2022-08-29 12:54:15 +0100

    Enabled Google Auth

--- api/v1/controllers/User/GetUser.js
+++ api/v1/controllers/User/GetUser.js
@@ -3,43 +3,67 @@ const { Op } = require('sequelize');
 const bcrypt = require('bcrypt');
 
 const GetUser = async (req, res) => {
-    const { id, email, password, login }  = req.body;
+    const { id, email, password, login, sign_in_method }  = req.body;
 
     if(email && password && login){
-        await User.findOne({
-            where: {
-                email: email
-            }
-        })
-        .then(async(data) => {
-
-            if(data === null){
-                res.json({ response: 'Account does not exist or your details are incorrect' })
-            } else {
-                const passwordsMatch = await bcrypt.compare(password, data.password)
-
-                if(passwordsMatch){
-                    return res.json({ 
-                        response: data,
-                        message: 'Account found!'
-                    })
-                } else {
-                    return res.status(401).json({ message: 'Password Incorrect' })
+        if(sign_in_method === 'email'){
+            await User.findOne({
+                where: {
+                    email: email
                 }
+            })
+            .then(async(data) => {
+    
+                if(data === null){
+                    return res.json({ response: 'Account does not exist or your details are incorrect' })
+                } 
+                else if(data.password === null){
+                    return res.status(400).json({ message: "Password does not exist for this account" })
+                }
+                else {
+                    const passwordsMatch = await bcrypt.compare(password, data.password)
+    
+                    if(passwordsMatch){
+                        return res.json({ 
+                            response: data,
+                            message: 'Account found!'
+                        })
+                    } else {
+                        return res.status(401).json({ message: 'Password Incorrect' })
+                    }
+    
+                }
+            })
+            .catch(() => res.json({ response: 'There was a problem finding your account' }))
+        } else if(sign_in_method === 'google') {
 
-            }
-        })
-        .catch(() => res.json({ response: 'There was a problem finding your account' }))
+            await User.findByPk(id)
+            .then(data => {
+                return res.json({ user_data: data })
+            })
+            .catch(err => {
+                return res.json({ message: 'Could not get user data!', code: err })
+            })
+        } 
+        else {
+            return res.status(400).json({ response: "Invalid sign in method" })
+        }
+
     } 
     else if(!login) {
+
         if(id){
-            await User.findByPk(id).then(data => {
-                res.json({ response: data })
+            await User.findByPk(id, { 
+                attributes: { exclude: ['email', 'password'] }
+            }).then(data => {
+                return res.json({ user_data: data })
             })
-            .catch(() => res.json({ response:'Error while getting user!'}))
-        } else {
+            .catch((err) => res.json({ response:'Error while getting user!', code:err }))
+        } 
+        else {
             return res.staus(400).json({ response:'Data missing!' })
-        }  
+        } 
+
     } 
 }
 
