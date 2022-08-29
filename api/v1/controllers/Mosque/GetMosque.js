43be6c253cecfd00c9ad0d75182928210a3341ba
fortune710 <fortune0208@yahoo.com>
2022-08-29 15:48:57 +0100

    added check mosque following to get mosque

--- api/v1/controllers/Mosque/GetMosque.js
+++ api/v1/controllers/Mosque/GetMosque.js
@@ -1,5 +1,27 @@
 const { Mosque } = require('../../../models/Mosque');
 const { PrayerTime } = require('../../../models/PrayerTime');
+const { Followership } = require('../../../models/Followership');
+const { Op } = require('sequelize');
+
+
+const checkIfUserIsFollowing = async(user_id, mosque_id) => { // Should return false if user is not following
+
+    const isFollowing = await Followership.findOne({
+        where: {
+            [Op.and]: [
+                { user_id: user_id },
+                { mosque_id: mosque_id }
+            ]
+        }
+    })
+    .then(data => {
+        if(data === null)
+            return false
+        else 
+            return true
+    })
+    return isFollowing;
+}
 
 
 const getPrayerTimes = async (id) => {
@@ -16,15 +38,17 @@ const getPrayerTimes = async (id) => {
 
 const GetMosque = async(req, res) => {
     const { id } = req.params;
+    const { user_id, mosque_id } = req.body;
     const prayers = await getPrayerTimes(id);
+    const isFollowing = await checkIfUserIsFollowing(user_id, mosque_id)
 
-    if(!id){
-        res.json({ response:'Data missing!' })
-        return
+    if(!id || !req.body){
+        return res.json({ response:'Data missing!' })
     } else {
         return await Mosque.findByPk(id).then(data => {
             const mosqueData = {
                 ...data.dataValues,
+                is_following: isFollowing,
                 prayer_times: prayers
             }
             res.json({ response: mosqueData })
