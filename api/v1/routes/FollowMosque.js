fe194f4a3193ba007c3301d760cab73ff48630b5
fortune710 <fortune0208@yahoo.com>
2022-09-06 11:01:39 +0100

    Added route to delete follower

--- api/v1/routes/FollowMosque.js
+++ api/v1/routes/FollowMosque.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const { GetFollow }  = require('../controllers/Followership/GetFollow');
 const { AddFollower } = require('../controllers/Followership/AddFollower');
+const { DeleteFollower } = require('../controllers/Followership/DeleteFollower')
 const { body } = require('express-validator');
 
 router.use(function(req, res, next) {
@@ -10,17 +11,21 @@ router.use(function(req, res, next) {
     next();
 });
 
-router.get('check', [
+router.get('/check', [
     body('user_id').not().isEmpty().trim(),
     body('mosque_id').not().isEmpty().trim(),
 ], (req, res) => GetFollow(req, res) )
 
 
-router.put('add', [
+router.put('/add', [
     body('user_id').not().isEmpty().trim(),
     body('mosque_id').not().isEmpty().trim(),
 ], (req, res) => AddFollower(req, res))
 
+router.delete('/remove',[
+    body('user_id').not().isEmpty().trim(),
+    body('mosque_id').not().isEmpty().trim(),
+], (req,res) => DeleteFollower(req, res))
 
 module.exports = {
     FollowRouter: router
