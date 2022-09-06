e739c182d6c5fe4bf03113948973103f2354a91d
fortune710 <fortune0208@yahoo.com>
2022-09-06 11:11:04 +0100

    Added function to check if the user is already

--- api/v1/controllers/Followership/AddFollower.js
+++ api/v1/controllers/Followership/AddFollower.js
@@ -1,10 +1,32 @@
 const { Followership } = require('../../../models/Followership');
 
+const checkIfFollowing = async(user_id, mosque_id) => {
+    const isFollowing = await Followership.findOne({
+        where: {
+            user_id: user_id,
+            mosque_id: mosque_id
+        }
+    })
+    .then(data => {
+        if(data){
+            return true
+        } else {
+            return false
+        }
+    })
+    return isFollowing
+}
 
 const AddFollower = async(req, res) => {
+    const { user_id, mosque_id } = req.body;
+
     if(!req.body){
         res.json({ response:'Data missing!' })
-    } else {
+    } 
+    else if(checkIfFollowing(user_id, mosque_id)){
+        return res.json({ following: checkIfFollowing(user_id, mosque_id),  message: "Already following!" })
+    } 
+    else {
         await Followership.create({
             ...req.body
         })
