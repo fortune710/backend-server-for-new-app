20dc2c6d28a4b3cb1f495013f609761f493f881a
fortune710 <fortune0208@yahoo.com>
2022-09-06 11:01:26 +0100

    controller to delete follower

---
+++ api/v1/controllers/Followership/DeleteFollower.js
@@ -0,0 +1,23 @@
+const { Followership } = require('../../../models/Followership');
+
+const DeleteFollower = async(req, res) => {
+    const { user_id, mosque_id } = req.body;
+    
+    await Followership.destroy({
+        where: {
+            user_id: user_id,
+            mosque_id: mosque_id
+        }
+    })
+    .then(data => {
+        return res.json({ following: false, message: 'Mosque unfollowed sucessfully' })
+    })
+    .catch((err) => {
+        return res.status(400).json({ error: err, message: "Could not unfollow mosque" })
+    })
+
+}
+
+module.exports = {
+    DeleteFollower: DeleteFollower
+}
