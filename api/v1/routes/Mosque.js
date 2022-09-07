db191fed1eb2185f4be0db3ad8e3e24a67a3db02
fortune710 <fortune0208@yahoo.com>
2022-09-07 08:48:46 +0100

    added cache control headers

--- api/v1/routes/Mosque.js
+++ api/v1/routes/Mosque.js
@@ -29,13 +29,14 @@ router.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Cache-Control", "no-store,no-cache,must-revalidate")
+    res.header("Pragma", "no-cache")
     next();
 });
 
 router.post('/create', (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
     res.setHeader("Access-Control-Allow-Headers", "content-type");
     res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
     AddMosque(req, res)
