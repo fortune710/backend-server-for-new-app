86a47f0a8d6a06c7ca7406288fdb27888ef625b8
fortune710 <fortune0208@yahoo.com>
2022-10-03 20:16:31 +0100

    configfor node mailer

---
+++ api/config/mail.config.js
@@ -0,0 +1,16 @@
+const nodemailer = require('nodemailer');
+
+const testAccount = await nodemailer.createTestAccount()
+const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false,
+    auth: {
+        user: testAccount.user,
+        pass: testAccount.pass
+    }
+})
+
+module.exports = {
+    transporterForEmail: transporter
+}
