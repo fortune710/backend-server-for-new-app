d6127293751bcd8924ed69b70eaf082db6fd8c40
fortune710 <fortune0208@yahoo.com>
2022-10-08 22:00:05 +0100

    changes

--- api/v1/routes/Auth.js
+++ api/v1/routes/Auth.js
@@ -10,7 +10,7 @@ const { ActivateAccount } = require('../controllers/User/ActivateAccount');
 
 AuthRouter.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
