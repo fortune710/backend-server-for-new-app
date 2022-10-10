8a0321620310ba9de19fa1d8a265c9b82b54f9f0
fortune710 <fortune0208@yahoo.com>
2022-10-10 16:06:55 +0100

    removed headers

--- index.js
+++ index.js
@@ -21,12 +21,14 @@ app.use(cors({
     credentials: true
 }))
 
+/*
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin");
     next();
 });
+*/
 
 app.use(bodyparser)
 
