26ddc65779f36ecf01e24e94338eb9c38abcbf2a
fortune710 <fortune0208@yahoo.com>
2022-10-10 15:39:17 +0100

    added new header

--- api/v1/routes/Mosque.js
+++ api/v1/routes/Mosque.js
@@ -29,7 +29,7 @@ router.all('/create', cors(corsOpts))
 router.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin");
     next();
 });
 
