3069b029d34f24452ded7b19a95243d9fe9a0563
fortune710 <fortune0208@yahoo.com>
2022-10-14 13:51:16 +0100

    controller to send email

---
+++ api/v1/controllers/User/SendEmail.js
@@ -0,0 +1,31 @@
+const { transporterForEmail } = require('../../../config/mail.config');
+
+const SendEmail = async (req, res) => {
+    const { user_id, email } = req.body;
+
+    transporterForEmail.sendMail({
+        subject: "Activate your Iqama Account",
+        from: "noreply@iqama.app",
+        to: email,
+        html: `
+        <h1>Activate your account</h1>
+
+        <p>Click the link below to activate your account</p>
+
+        <button href="https://iiqama-api.herokuapp.com/auth/activate/${user_id}">
+            Activate Account
+        </button>
+        `
+    })
+    .then(() => {
+        return res.json({ message: "Email sent sucessfully!" })
+    })
+    .catch(() => {
+        return res.status(400).json({ message: "Error while sending email!" })
+    })
+
+}
+
+module.exports = {
+    SendEmail: SendEmail
+}
